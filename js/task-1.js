54696ed0038976da61aa937036e9984952d0626a
Neoriya <100101631+Neoriya@users.noreply.github.com>
2024-02-28 00:15:59 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,29 @@
+function isEnoughCapacity(products, containerSize) {
+  const allProductsValues = Object.values(products);
+  let productsCounter = 0;
+  for (const product of allProductsValues) {
+    productsCounter += product;
+  }
+  //   console.log(productsCounter);
+  return containerSize >= productsCounter ? true : false;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+
+// якщо це корректно, чи можу я тут задати питання?
+// коли роблю console.log(productsCounter), return при цьому закоментовано, результат такий:
+// 6;
+// undefined;
+// 26;
+// undefined;
+// 9;
+// undefined;
+// 25;
+// undefined;
+// звідки беруться ці undefined?
